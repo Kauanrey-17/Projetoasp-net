a51155bc52700c086cac2339c992983be340c458
Kauan <reykauan6@gmail.com>
2025-05-16 20:16:37 -0300

    Criando a injeção de dependencia do prjeto

--- Projetoasp.net/Program.cs
+++ Projetoasp.net/Program.cs
@@ -1,9 +1,14 @@
+using Projetoasp.net.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddScoped<LoginRepositorio>();
+builder.Services.AddScoped<ProdutoRepositorio>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
