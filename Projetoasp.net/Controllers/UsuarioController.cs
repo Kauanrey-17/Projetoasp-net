8a23f07d7f9f334032e5868e7501d6a42a0bf062
Kauan <reykauan6@gmail.com>
2025-05-16 20:12:37 -0300

    Criando as controllers do projeto

---
+++ Projetoasp.net/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projetoasp.net.Controllers
+{
+    public class Usuario : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
